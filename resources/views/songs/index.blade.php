7cde7fd49e3d37385a3fff0be49d9548947d3003
StefanoMRC <stefano.mrc92@gmail.com>
2022-06-06 17:45:51 +0200

    fine index

--- resources/views/songs/index.blade.php
+++ resources/views/songs/index.blade.php
@@ -1,4 +1,30 @@
 @extends('layout.layout')
 @section('cont')
-    <h1>ciao</h1>
+<table class="table">
+    <thead>
+        <tr>
+            <th scope="col" class="text-capitalize">copertina</th>
+            <th scope="col" class="text-capitalize">autore</th>
+            <th scope="col" class="text-capitalize">titolo</th>
+            <th scope="col" class="text-capitalize">genere</th>
+            <th scope="col" class="text-capitalize">anno di uscita</th>
+            <th scope="col" class="text-capitalize">stato di salute</th>
+        </tr>
+    </thead>
+    <tbody>
+        @forelse ($songs as $song )
+        <tr>
+            <td>
+                <img src="{{ $song->poster }}" alt="" width="100px">
+            </td>
+            <td>{{ $song->title }}</td>
+            <td>{{ $song->author }}</td>
+            <td>{{ $song->genre }}</td>
+            <td>{{ $song->year }}</td>
+            <td>{{ $song->status }}</td>
+
+        </tr>
+        @empty
+
+        @endforelse
 @endsection
