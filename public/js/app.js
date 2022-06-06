ada4434b4d873550f68080edf48a43f86ac745db
Gerardo Calia <sofesso97@gmail.com>
2022-06-06 21:08:42 +0200

    Pulsante elimina completo

--- public/js/app.js
+++ public/js/app.js
@@ -24190,8 +24190,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Roberto\Boolean\laravel-festival-crud\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Roberto\Boolean\laravel-festival-crud\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\sofes\Documents\Coding\classe_58\laravel-festival-crud\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\sofes\Documents\Coding\classe_58\laravel-festival-crud\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
